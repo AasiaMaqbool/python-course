2870a5912cd5f587af9e7d6cf196dc9ec111de26
AasiaMaqbool <aasimaqbool@gmail.com>
2024-05-31 21:30:39 +0500

    immutabilty-of-str

---
+++ immutabilty_example.py
@@ -0,0 +1,10 @@
+A = 25
+print(A)
+A = 30
+print(A)
+name = 'a'
+print(type(name))
+print(name)
+# try to change the sequence of str but python does not support it bcoz of immutablity
+name[0] = 'u'
+print(name)
