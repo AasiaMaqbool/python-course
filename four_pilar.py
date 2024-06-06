a5dee1e48ea4402ae8fcff5ff82b1bf073c2804a
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-06 19:02:17 +0500

    Abstactration

--- four_pilar.py
+++ four_pilar.py
@@ -1,17 +1,16 @@
-class Family:
-    def __init__(self,family_name,number_of_members,country):
-        self.family_name=family_name
-        self.number_of_members=number_of_members
-        self.country=country
-    def member_says(self):
-        print(f"Hey,I am from{self.family_name} family and there are {self.number_of_members}members in family")
-class Family_detailed(Family):
-    def which_country(self):
-        print(f"The {self.family_name} family has roots from {self.country}")
-a=Family("Rodrigue",5,"pern")
-b=Family_detailed("Bezos",15,"UAE")
+from abc import ABC,abstractmethod
+class company(ABC):
+    def work(self):
+       pass
+class Manager(company):
+    def work(self):
+        print("I assign work to mange team")
+class Employee(company):
+    def work(self):
+       print("I complete the work assign to me")
 
-a.member_says()
-b.member_says()
-b.which_country()
+R=Manager()
+R.work
 
+K=Employee()
+K.work()
