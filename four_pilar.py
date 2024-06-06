91eb86fe2b3b178602d7c7aa0b65d326b8fb8d25
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-06 18:48:22 +0500

    Inheritance

--- four_pilar.py
+++ four_pilar.py
@@ -1,11 +1,17 @@
-class Cat:
-    def __init__(self):
-        self._sound="meow"
-    def speak(self):
-        print("cat says:{}".format(self._sound))
+class Family:
+    def __init__(self,family_name,number_of_members,country):
+        self.family_name=family_name
+        self.number_of_members=number_of_members
+        self.country=country
+    def member_says(self):
+        print(f"Hey,I am from{self.family_name} family and there are {self.number_of_members}members in family")
+class Family_detailed(Family):
+    def which_country(self):
+        print(f"The {self.family_name} family has roots from {self.country}")
+a=Family("Rodrigue",5,"pern")
+b=Family_detailed("Bezos",15,"UAE")
 
-c=Cat()
-c.speak()
-#when changes in prices
-c.sound="bow.wow"
-c.speak()
+a.member_says()
+b.member_says()
+b.which_country()
+
