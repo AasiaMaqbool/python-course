930def5de99e7332a5e6af485f723f2808d016d4
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-07 18:43:10 +0500

    map

--- pure_function.py
+++ pure_function.py
@@ -1,7 +1,4 @@
-def multiply_by2(li):
-    new_list=[]
-    for item in li:
-        new_list.append(item*2)
-    return new_list
+def multiply_by2(item):
+    return item*2
 
-print(multiply_by2([1,2,3]))
+print(list(map(multiply_by2,[1,2,3])))
