6f867375bf99eab6a01fc688c9394c347a608671
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-08 10:43:34 +0500

    return

--- map.py
+++ map.py
@@ -4,5 +4,9 @@ def multiply_by2(item):
     return item*2
 def only_odd(item):
     return item % 2 != 0
+def add(a,b):
+    return a +b;
+      result= add(2,3)'n
 print(list(zip(your_list,my_list)))
 print(my_list)
+print('result')
