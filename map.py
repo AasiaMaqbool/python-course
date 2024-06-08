d9036fdc90eb59739ea99d8e3882a7c050c704c3
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-08 11:44:37 +0500

    lambda

--- map.py
+++ map.py
@@ -1,12 +1,6 @@
+
+from functools import reduce
 my_list=[1,2,3]
-your_list=[10,20,30]
-def multiply_by2(item):
-    return item*2
-def only_odd(item):
-    return item % 2 != 0
-def add(a,b):
-    return a +b;
-      result= add(2,3)'n
-print(list(zip(your_list,my_list)))
+
+print(list(map(lambda items:items*2,my_list)))
 print(my_list)
-print('result')
