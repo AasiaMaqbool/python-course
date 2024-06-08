a37f7afe5a18cfd3e56c2ba26a95e0d96451d860
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-08 11:55:23 +0500

    comprenhsion_in_python

--- map.py
+++ map.py
@@ -1,6 +1,12 @@
+my_list=[char for char in 'hello']
+my_list2=[num for num in range(0,50)]
+my_list3=[num*2 for num in range(0,50) ]
+my_list4=[num**2 for num in range(0,50) if  num%2==0]
 
-from functools import reduce
-my_list=[1,2,3]
 
-print(list(map(lambda items:items*2,my_list)))
+
+
 print(my_list)
+print(my_list2)
+print(my_list3)
+print(my_list4)
