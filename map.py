2e79218fac14b726b628960147f9d3629e179894
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-08 10:38:59 +0500

    zip

--- map.py
+++ map.py
@@ -1,7 +1,8 @@
 my_list=[1,2,3]
+your_list=[10,20,30]
 def multiply_by2(item):
     return item*2
 def only_odd(item):
     return item % 2 != 0
-print(list(filter(only_odd,my_list)))
+print(list(zip(your_list,my_list)))
 print(my_list)
