4b13899b172d1d343cb9df6dcc188972740f5417
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-12 12:32:07 +0500

    practise here to creation function

---
+++ functionUnderstanding.py
@@ -0,0 +1,31 @@
+val1 = 5
+val2 = 6
+
+val3 = val1 + val2
+
+print(val3)
+
+val4 = 7
+val5 = 8
+
+val6 = val4 + val5
+
+print(val6)
+
+
+def add(v1, v2):
+    return v1 + v2
+
+
+def tadd(v1, v2):
+    return v1 + v2
+
+
+vt = add(5, 6)
+print(vt)
+vu = add(7, 8)
+print(vu)
+
+print(tadd(2, 3))
+
+
---
+++ modules.py
@@ -0,0 +1,8 @@
+#import generator
+
+#print(generator)
+import shopping_cart.shopping
+print(shopping_cart.shopping.buy('apple'))
+import shopping_cart.ulatity
+print(shopping_cart.ulatity.multiply(3 , 5))
+#print(multiply(3,5))
