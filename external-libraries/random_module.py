5160b565108bcceab82201150500306423e53304
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-20 17:51:55 +0500

    random_game

--- external-libraries/random_module.py
+++ external-libraries/random_module.py
@@ -1,1 +1,21 @@
-import random
+from random import randint
+import sys
+answer=randint(1, 10)
+#answer= randint(int(sys.argv[1]),int(sys.argv[2]))
+
+while True:
+     try:
+         guess=int(input('guess a number  1 ~ 10  '))
+         if 0 < guess < 11:
+             if guess == answer:
+                 print('you are a genius')
+                 break
+         else:
+            print('hey bazo, I say 1 ~ 10  ')
+     except ValueError:
+        print('please enter a number')
+        continue
+
+
+
+
