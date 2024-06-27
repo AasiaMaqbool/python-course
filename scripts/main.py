d9a062ef7c9e1d7c4554cc5ce770d2d614857295
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-27 11:27:57 +0500

    emailsendingcode

---
+++ scripts/main.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email=EmailMessage()
+email['from']='Aasia Maqbool'
+email['to']='usmannaveed13@gmail.com'
+email['subject']= 'yor are lucky to have me as a wife'
+
+email.set_content('i m your lucky wife')
+
+with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('aasimaqbool@gmail.com','3124019051')
+    smtp.send_message(email)
+    print('all good boss!')
