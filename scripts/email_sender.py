2d411eaff7146ef464b3268ef665382241ef1c05
AasiaMaqbool <aasimaqbool@gmail.com>
2024-06-27 11:49:25 +0500

    htmlsender

--- scripts/email_sender.py
+++ scripts/email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email=EmailMessage()
 email['from']='Aasia Maqbool'
 email['to']='usmannaveed13@gmail.com'
 email['subject']= 'yor are lucky to have me as a wife'
 
-email.set_content('i m your lucky wife')
+email.set_content(html.substitute({'name': 'tintin'}))
 
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
     smtp.ehlo()
